34021aa3421e4a33c965fd3c67de3b76eb92903a
Apoerakay <apoerak@gmail.com>
2024-02-29 04:10:59 -0800

    used useSWR fetch recipes

--- src/components/recipe-item/index.jsx
+++ src/components/recipe-item/index.jsx
@@ -1,29 +1,32 @@
 import React from "react";
-import{
-    Grid,
-    Card,
-    CardMedia,
-    CardContent,
-    Typography,
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
 } from "@mui/material"
+import { Link } from "react-router-dom";
 
-export default function RecipeItem( {image, title}) {
-    return(
-        <>
-         <Grid item xs={4}>
-         <Card sx={{ maxWidth: 345, height: '100%' }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={image}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {title}
-        </Typography>
-      </CardContent>
-    </Card>
-       </Grid>
-        </>
-    )
+export default function RecipeItem({ image, title, id }) {
+  return (
+    <>
+      <Grid item xs={4}>
+        <Card sx={{ maxWidth: 345, height: '100%' }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={image}
+            title="green iguana"
+          />
+          <CardContent>
+            <Link to={`/recipes/${id}`}>
+              <Typography gutterBottom variant="h5" component="div">
+                {title}
+              </Typography>
+            </Link>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  )
 }
--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -6,53 +6,54 @@ import spinner from "../../assets/images/infinite-spinner.svg"
 
 
 
-export default function Recipes(){
-    const [recipes, setRecipes] = useState([]);
-   const [ searchItem, setSearchItem] = useState("");
-   const [loading, setLoading] = useState(false);
-   
-    const searchRecipes = () => {
+export default function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
+  const [loading, setLoading] = useState(false);
 
-      setLoading(true);
+  const searchRecipes = () => {
 
-        const url = new URL( "https://api.spoonacular.com/recipes/complexSearch");
-        url.searchParams.append('apiKey',process.env.REACT_APP_SPOONACULAR_API_KEY);
-        url.searchParams.append('query', searchItem);
+    setLoading(true);
 
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-         //update recipes state
-         //console.log({data});
-         setRecipes(data.results)
+    const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+    url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+    url.searchParams.append('query', searchItem);
 
-        })
-        .catch((error) => {console.log (error);
-        }).finally(() => setLoading(false))
-    };
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        //update recipes state
+        //console.log({data});
+        setRecipes(data.results)
 
-    useEffect(searchRecipes, [])
+      })
+      .catch((error) => {
+        console.log(error);
+      }).finally(() => setLoading(false))
+  };
 
-    return(
-        <Container sx={{my: '2rem'}}>
-     <TextField 
-     fullWidth
-      id="outlined-basic" 
-      label="Enter a keyword to search recipes and hit enter" 
-      variant="outlined"
-      value={searchItem}
-      onChange={(event) => setSearchItem(event.target.value)}
-      onKeyDown={event => event.key == 'Enter' && searchRecipes()}
-       />
+  useEffect(searchRecipes, [])
+
+  return (
+    <Container sx={{ my: '2rem' }}>
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Enter a keyword to search recipes and hit enter"
+        variant="outlined"
+        value={searchItem}
+        onChange={(event) => setSearchItem(event.target.value)}
+        onKeyDown={event => event.key == 'Enter' && searchRecipes()}
+      />
 
 
-      <Grid sx={{mt:'1rem'}} container spacing={3}>
-      {loading ? <Container sx={{display: 'flex', justifyContent: 'center'}}><img src={spinner} width='25%'/></Container> : recipes.length > 0 ? recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image}/>) : 
-      (<Container sx={{display: 'flex', justifyContent: 'center'}}>
-      <img src={noRecipes} width="25%"/>
-        </Container>)}
+      <Grid sx={{ mt: '1rem' }} container spacing={3}>
+        {loading ? <Container sx={{ display: 'flex', justifyContent: 'center' }}><img src={spinner} width='25%' /></Container> : recipes.length > 0 ? recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image} id={recipe.id} />) :
+          (<Container sx={{ display: 'flex', justifyContent: 'center' }}>
+            <img src={noRecipes} width="25%" />
+          </Container>)}
 
       </Grid>
-        </Container>
-    );
+    </Container>
+  );
 }
